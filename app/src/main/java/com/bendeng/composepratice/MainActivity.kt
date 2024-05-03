f20f53f686b98bc6aec7f857f41a87ebafe6d48a
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-05-03 22:02:28 +0900

    Pratice Modifier

--- app/src/main/java/com/bendeng/composepratice/MainActivity.kt
+++ app/src/main/java/com/bendeng/composepratice/MainActivity.kt
@@ -3,41 +3,44 @@ package com.bendeng.composepratice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.bendeng.composepratice.ui.theme.ComposePraticeTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposePraticeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+            GreetingText("world")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun GreetingText(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier
+            .clickable(onClick = { })
+            .padding(all = 10.dp)
+            .width(200.dp)
+            .height(240.dp),
+        style = MaterialTheme.typography.headlineMedium,
+        fontWeight = FontWeight.SemiBold
+
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposePraticeTheme {
-        Greeting("Android")
-    }
+fun GreetingPreviewMainActivity() {
+    GreetingText("world")
 }
